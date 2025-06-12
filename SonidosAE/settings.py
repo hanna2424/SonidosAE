7afbb90ff2ff089846a6c56f9bac44a017e16c8a
Jhanely Armijos <armijosalejandra2001@gmail.com>
2025-06-11 21:46:58 -0500

    mi sexto commit configurando media para archivos

--- SonidosAE/settings.py
+++ SonidosAE/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#configuracion de archivos
+MEDIA_URL='/media/'
+MEDIA_ROOT=(os.path.join(BASE_DIR,'SonidosAE/media/'))
+
+X_FRAME_OPTIONS= 'SAMEORIGIN'
+
--- SonidosAE/urls.py
+++ SonidosAE/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+#para mostrar los archivos en los templates 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('', include('Aplicaciones.AnalisisSonidos.urls')),
+    #path('', include('Aplicaciones.Especies.urls')),
+    #path('', include('Aplicaciones.EspeciesSonidos.urls')),
+    #path('', include('Aplicaciones.GrabacionSonidos.urls')),
 ]
+
+if settings.DEBUG:urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
